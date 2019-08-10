6b71f15429509974d59146398d04dd4963795d0a
shubham <shubham@FF-DSK-60.fitnessforce.com>
2019-08-10 15:32:52 +0530

    Login Credential updated & element ID updated

--- gittest/src/test/java/guru99/gittest/AppTest.java
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -14,10 +14,10 @@ public class AppTest {
 	  
 	
 	wd.findElement(By.id("txtUsername")).sendKeys("fitnessforce.support");
-	wd.findElement(By.id("txtPassword")).sendKeys("fit@1235");
+	wd.findElement(By.id("txtPassword")).sendKeys("fit@123");
 	//wd.findElement(By.id("txtUsername")).sendKeys("admin");// enter username
 	//wd.findElement(By.id("txtPassword")).sendKeys("Member@123");// enter password
-	wd.findElement(By.id("SignInt")).click();// click on sign in
+	wd.findElement(By.id("SignIn")).click();// click on sign in
 	  System.out.println("test");
 	  
 	  System.out.println("New Branch");
