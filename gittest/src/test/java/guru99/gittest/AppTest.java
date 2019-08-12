1400d85360b0f193c764b12f49b24fd9a8e5e6ef
gripsunil <53944411+gripsunil@users.noreply.github.com>
2019-08-12 16:54:18 +0530

    updated

--- gittest/src/test/java/guru99/gittest/AppTest.java
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
-public class Fitnessforcetest {
+public class AppTest {
 	WebDriver wd;
 	WebDriverWait wait;
   @Test
