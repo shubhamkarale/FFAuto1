23947406da7e573ffb49c9831b68eff283452d81
gripsunil <53944411+gripsunil@users.noreply.github.com>
2019-08-22 19:29:36 +0530

    code cleaning

--- gittest/src/test/java/guru99/gittest/Membershipbill_productSelection.java
+++ gittest/src/test/java/guru99/gittest/Membershipbill_productSelection.java
@@ -27,11 +27,6 @@ public class Membershipbill_productSelection extends Setup {
 		  wd.findElement(By.id("AddProdBtn")).click();
 		  wd.findElement(By.id("Item_0_0_132691")).click();
 		  wd.findElement(By.id("AddProdBtn")).click();
-//		  wd.findElement(By.xpath("//*[@id='Item_0_0_132723']/div")).click();
-//		  wd.findElement(By.id("AddProdBtn")).click();
-//		  wd.findElement(By.xpath("//*[@id='Item_0_0_132726']/div")).click();
-//		  wd.findElement(By.id("AddProdBtn")).click();
-
 
 	}
   
@@ -55,46 +50,25 @@ public class Membershipbill_productSelection extends Setup {
 	 	Thread.sleep(5000);	
 	 	
 	 	String MemberID = wd.getCurrentUrl().substring(80, 88);
-	 	
-//	 	System.out.println(CurrentUrl);
 
 	 	ArrayList<String> tabs4 = new ArrayList<String>(wd.getWindowHandles());
 		int tabcount = tabs4.size();
-//		System.out.println(tabcount);
+
 		wd.switchTo().window(tabs4.get(tabcount - 1));	
-		
-//	 	System.out.println(" Switch to Pop up page ");
 		
 	 	Robot robot = new Robot();
 	 	Thread.sleep(3000);
-	 	
-//		System.out.println(" Robot Started ");
 		
 	 	robot.keyPress(KeyEvent.VK_ESCAPE);
 	 	
 	 	robot.keyRelease(KeyEvent.VK_ESCAPE);
 	 	
-//	 	System.out.println(" Robot Ended ");
-	 	
 		ArrayList<String> tabs5 = new ArrayList<String>(wd.getWindowHandles());
 		int tabcount2 = tabs5.size();
-//		System.out.println(tabcount2);
-		wd.switchTo().window(tabs5.get(tabcount2 - 1));
-//	 	
-//	 	System.out.println(Window2);
-//	 	
-//	 	wd.switchTo().window(tabs5.get(0)).close();
-//	 	
-//	 	ArrayList<String> tabs6 = new ArrayList<String>(wd.getWindowHandles());
-//	 	int Window3 = tabs6.size();
-//	 	
-//	 	System.out.println(Window3);
-//	 	
-//	 	wd.close();
-	 	
-	 	String b = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/table/tbody/tr[3]/td/table[1]/tbody/tr/td[2]/div[2]"))).getText().substring(1);
 
-//	 	System.out.println(b);
+		wd.switchTo().window(tabs5.get(tabcount2 - 1));
+
+	 	String b = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/table/tbody/tr[3]/td/table[1]/tbody/tr/td[2]/div[2]"))).getText().substring(1);
 
 	 	wd.get("https://demo.fitnessforce.com/clientview.aspx?pagevalue=1&enquiryid="+MemberID+"&tenantid=460");
 	 	
@@ -105,18 +79,8 @@ public class Membershipbill_productSelection extends Setup {
 	 	String Billid = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html[1]/body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"
 	 			+ "/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/span[1]/span[1]"))).getText();
 	 	
-//	 	System.out.println(Billid.length());
-	 	
-//		String[] arrSplit = Billid.split(" | ");
-//		     
-//		System.out.println(arrSplit[0]);
-//
-//	 	String trim = arrSplit[0].trim();
-	 	
 	 	String newStr = Billid.substring(0, Billid.indexOf(" | ")).trim();
-	 	
-//	 	System.out.println(newStr.length());
-	 	
+
 	 	if(b.matches(newStr)) {
 	 		
 	 	System.out.println("Bill Created SuccessFully. Your Bill Id - " + b);
@@ -125,64 +89,6 @@ public class Membershipbill_productSelection extends Setup {
 	 	
 	 		System.out.println("Bill Not Found or Failed for Member ID - " + MemberID);
 	 	}
-	 	
-	 	
-//	 	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Purchase_History"))).click();
-//	 	
-//	 	if(wd.findElements(By.className("ch-bill")).size() != 0) {
-//	 		
-//	 		System.out.println("Bill Is not Created");
-//	 	} else {
-//	 		
-//	 		String Billid = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html[1]/body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/span[1]/span[1]"))).getText();
-//	 	System.out.println(Billid);
-//	 	
-//	 	}
-//	 	
-	 	
-		
-//	 	Robot robot = new Robot();
-//	 	Thread.sleep(1000);
-//	 	
-//	 	System.out.println(" Switch to Pop up page ");
-//		
-//		System.out.println(" Robot Started ");
-//	 	robot.keyPress(KeyEvent.VK_ESCAPE);
-//	 	robot.keyRelease(KeyEvent.VK_ESCAPE);
-//	 	
-//	 	System.out.println(" Robot Ended ");
-//
-//	 	
-
-//	 	
-//	 	wd.get("https://demo.fitnessforce.com/clientview.aspx?pagevalue=1&enquiryid="+CurrentUrl+"&tenantid=460");
-//	 	
-
-	 	
-//	 	Robot robot = new Robot();
-//	 	Thread.sleep(1000);
-//	 	
-//	 	System.out.println(" Switch to Pop up page ");
-//		
-//		System.out.println(" Robot Started ");
-//	 	robot.keyPress(KeyEvent.VK_ESCAPE);
-//	 	robot.keyRelease(KeyEvent.VK_ESCAPE);
-//	 	
-//	 	System.out.println(" Robot Ended ");
-//	 	
-//	 	
-//		ArrayList<String> tabs4 = new ArrayList<String>(wd.getWindowHandles());
-//		wd.switchTo().window(tabs4.get(0));
-//		
-//		System.out.println(" Switch to purchase page ");
-//		
-//		ArrayList<String> print = new ArrayList<String>(wd.getWindowHandles());
-//		wd.switchTo().window(print.get(1));
-//		
-//		Thread.sleep(2000);
-//
-
-	 	
   }
 	  
   }
