bffeb165fe4415d3c3c2cb1c0b051218c3c2f317
gripsunil <53944411+gripsunil@users.noreply.github.com>
2019-08-22 18:56:30 +0530

    bill id verification

--- gittest/src/test/java/guru99/gittest/Membershipbill_productSelection.java
+++ gittest/src/test/java/guru99/gittest/Membershipbill_productSelection.java
@@ -54,6 +54,110 @@ public class Membershipbill_productSelection extends Setup {
 	 	
 	 	Thread.sleep(5000);	
 	 	
+	 	String CurrentUrl = wd.getCurrentUrl().substring(80, 88);
+	 	
+	 	System.out.println(CurrentUrl);
+
+	 	ArrayList<String> tabs4 = new ArrayList<String>(wd.getWindowHandles());
+		int tabcount = tabs4.size();
+		System.out.println(tabcount);
+		wd.switchTo().window(tabs4.get(tabcount - 1));	
+		
+		
+	 	System.out.println(" Switch to Pop up page ");
+		
+	 	Robot robot = new Robot();
+	 	Thread.sleep(3000);
+	 	
+		System.out.println(" Robot Started ");
+		
+	 	robot.keyPress(KeyEvent.VK_ESCAPE);
+	 	
+	 	robot.keyRelease(KeyEvent.VK_ESCAPE);
+	 	
+	 	System.out.println(" Robot Ended ");
+	 	
+//	 	ArrayList<String> tabs5 = new ArrayList<String>(wd.getWindowHandles());
+//	 	int Window2 = tabs5.size();
+//	 	
+//	 	System.out.println(Window2);
+//	 	
+//	 	wd.switchTo().window(tabs5.get(0)).close();
+//	 	
+//	 	ArrayList<String> tabs6 = new ArrayList<String>(wd.getWindowHandles());
+//	 	int Window3 = tabs6.size();
+//	 	
+//	 	System.out.println(Window3);
+//	 	
+//	 	wd.close();
+	 	
+	 	String b = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/table/tbody/tr[3]/td/table[1]/tbody/tr/td[2]/div[2]"))).getText().substring(1);
+
+	 	System.out.println(b);
+
+	 	wd.get("https://demo.fitnessforce.com/clientview.aspx?pagevalue=1&enquiryid="+CurrentUrl+"&tenantid=460");
+	 	
+	 	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Purchase_History"))).click();
+	 	
+	 	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("createNewmembership")));
+	 	
+	 	String Billid = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html[1]/body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"
+	 			+ "/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/span[1]/span[1]"))).getText();
+	 	
+	 	System.out.println(Billid.length());
+	 	
+//		String[] arrSplit = Billid.split(" | ");
+//		     
+//		System.out.println(arrSplit[0]);
+//
+//	 	String trim = arrSplit[0].trim();
+	 	
+	 	String newStr = Billid.substring(0, Billid.indexOf(" | ")).trim();
+	 	
+	 	System.out.println(newStr.length());
+	 	
+	 	if(b.matches(newStr)) {
+	 		
+	 	System.out.println("Bill Created SuccessFully");
+	 	
+	 	}else {
+	 	
+	 		System.out.println("Bill Not Found");
+	 	}
+	 	
+	 	
+//	 	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Purchase_History"))).click();
+//	 	
+//	 	if(wd.findElements(By.className("ch-bill")).size() != 0) {
+//	 		
+//	 		System.out.println("Bill Is not Created");
+//	 	} else {
+//	 		
+//	 		String Billid = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html[1]/body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/span[1]/span[1]"))).getText();
+//	 	System.out.println(Billid);
+//	 	
+//	 	}
+//	 	
+	 	
+		
+//	 	Robot robot = new Robot();
+//	 	Thread.sleep(1000);
+//	 	
+//	 	System.out.println(" Switch to Pop up page ");
+//		
+//		System.out.println(" Robot Started ");
+//	 	robot.keyPress(KeyEvent.VK_ESCAPE);
+//	 	robot.keyRelease(KeyEvent.VK_ESCAPE);
+//	 	
+//	 	System.out.println(" Robot Ended ");
+//
+//	 	
+
+//	 	
+//	 	wd.get("https://demo.fitnessforce.com/clientview.aspx?pagevalue=1&enquiryid="+CurrentUrl+"&tenantid=460");
+//	 	
+
+	 	
 //	 	Robot robot = new Robot();
 //	 	Thread.sleep(1000);
 //	 	
@@ -76,33 +180,7 @@ public class Membershipbill_productSelection extends Setup {
 //		
 //		Thread.sleep(2000);
 //
-//	 	String b = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/table/tbody/tr[3]/td/table[1]/tbody/tr/td[2]/div[2]"))).getText().substring(1);
-//
-//	 	System.out.println(b);
-//		
-//		wd.close();
-//		
-//		ArrayList<String> main = new ArrayList<String>(wd.getWindowHandles());
-//		wd.switchTo().window(main.get(0));
-//	 	
-//	 	wd.findElement(By.id("Purchase_History")).click();
-//	 	
-//	 	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("createNewmembership")));
-//	 	
-//	 	String BillNo = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html[1]/body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"
-//	 			+ "/div[2]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]/div[1]/span[1]/span[1]"))).getText();
-//	 	
-//	 	System.out.println(BillNo);
-//	 	
-//	 	if(BillNo.contains(b)) {
-	 	System.out.println("Bill Created SuccessFully");
-	 	
-	 	
-	 	
-//	 	}else {
-//	 	
-//	 		System.out.println("Bill Not Found");
-//	 	}
+
 	 	
   }
 	  
